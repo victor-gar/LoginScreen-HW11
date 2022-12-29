0a59da059a149e16fcd3e6556f209509bc5ad9a0
Garitskiy-Victor <dinomc4711@gmail.com>
2022-12-29 15:49:50 +0600

    create Custom form

--- IOS8 - HW11 - Garitskiy Victor/ViewController.swift
+++ IOS8 - HW11 - Garitskiy Victor/ViewController.swift
@@ -18,3 +18,46 @@ class ViewController: UIViewController {
 
 }
 
+class CustomView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required  init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func setupView() {
+        let path: UIBezierPath = getPath()
+        let shape = CAShapeLayer()
+        
+        shape.path = path.cgPath
+        shape.lineWidth = 2.0
+        shape.strokeColor = UIColor.clear.cgColor
+        shape.fillColor = UIColor.white.cgColor
+        shape.shadowColor = UIColor.black.cgColor
+        shape.shadowOpacity = 0.1
+        shape.shadowOffset = .zero
+        shape.shadowRadius = 10
+        shape.shouldRasterize = true
+        shape.rasterizationScale = UIScreen.main.scale
+        self.layer.addSublayer(shape)
+    }
+    
+    func getPath() -> UIBezierPath {
+        let path: UIBezierPath = UIBezierPath()
+        let bounds = UIScreen.main.bounds
+
+        path.move(to: CGPoint(x: 0, y: 1000))
+        path.addLine(to: CGPoint(x: 0, y: 1000))
+        path.addLine(to: CGPoint(x: 0, y: 600))
+        path.addCurve(to: CGPoint(x: bounds.width + 100, y: 700),
+                      controlPoint1: CGPoint(x: 200, y: 700),
+                      controlPoint2: CGPoint(x: 350, y: 350))
+        path.addLine(to: CGPoint(x: 500, y: 1000))
+        path.close()
+        return path
+    }
+}
