7651e7a42210e20b5ac9c089caf52c155e5d3a97
Garitskiy-Victor <dinomc4711@gmail.com>
2022-12-29 15:07:42 +0600

    Initial Commit

---
+++ IOS8 - HW11 - Garitskiy Victor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS8 - HW11 - Garitskiy Victor
+//
+//  Created by Victor Garitskyu on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
